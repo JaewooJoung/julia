af9da1c2dbad90140f0fec0e520defdd3757c69b
Jaewoo Joung <89329303+JaewooJoung@users.noreply.github.com>
2021-12-19 16:02:02 +0800

    而立秘诀
    write your files so that you can record what was learned 😘

---
+++ 而立秘诀-1.jl
@@ -0,0 +1,12 @@
+你好="hello" #comment is very important 
+sp =' ' #for Character of one we only need ''
+世界= "world" #space is not important 
+exc = '!' #for charater we can use ''
+我 = 'I' # use space for readability
+是 = "'m" # ' can be inside of ""
+朱莉娅 = "Julia" #
+println(你好,sp,世界,exc,sp,我,是,sp,朱莉娅)
+println("$你好$sp$世界$exc$sp$我$是$sp$朱莉娅")
+println("$你好 $世界$exc $我$是 $朱莉娅")
+print("$你好 $世界$exc $(我*是) $朱莉娅 \n",)
+print("$你好 $世界$(exc^2) $(我*是) $朱莉娅 \r",)
