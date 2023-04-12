04df17b60bbab4a907db59b3f0620f37d534abf5
JaewooJoung <jaewoo.joung@outlook.com>
2023-04-12 11:31:09 +0200

    20230412113108 Auto push

--- gitit.jl
+++ gitit.jl
@@ -1,14 +1,14 @@
-#git config --global user.name "JaewooJoung"
-#git config --global user.email "jaewoo.joung@outlook.com"
-#git config --list # 계정확인
-#cd d: #로컬 위치
+#c:\tools\git\bin\git.exe config --global user.name "JaewooJoung"
+#c:\tools\git\bin\git.exe config --global user.email "jaewoo.joung@outlook.com"
+#c:\tools\git\bin\git.exe config --list # 계정확인
+#cd C:\Users\jaewoo.joung\ #로컬 위치
 #git clone https://github.com/JaewooJoung/julia#git pull origin main
 using Dates 
 my_message = "Auto push" 
-repo_path = "D:\\julia"
+repo_path = "C:\\Users\\jaewoo.joung\\julia"
 cd(repo_path)
-run(`git add .`)
+run(`c:\\tools\\git\\bin\\git.exe add .`)
 dt=now()
 commit_message = Dates.format(dt, "yyyymmddHHMMSS ") * my_message
-run(`git commit -m $commit_message`)
-run(`git push --all`)
+run(`c:\\tools\\git\\bin\\git.exe commit -m $commit_message`)
+run(`c:\\tools\\git\\bin\\git.exe push --all`)
