b0ad9bd058c6ab3320e11d0a7f2de27b14eb3ea1
JaewooJoung <jaewoo.joung@outlook.com>
2023-04-12 13:26:20 +0200

    20230412132620 dude check the 14th line here

--- gitit.jl
+++ gitit.jl
@@ -4,12 +4,12 @@
 #cd C:\Users\jaewoo.joung\ #local drive 
 #git clone https://github.com/JaewooJoung/julia#git pull origin main
 using Dates 
-my_message = "today is my first commit in CEVT" 
+my_message = "dude check the 14th line here " 
 repo_path = "C:\\Users\\jaewoo.joung\\julia"
 cd(repo_path)
 run(`c:\\tools\\git\\bin\\git.exe add .`)
 dt=now()
 commit_message = Dates.format(dt, "yyyymmddHHMMSS ") * my_message
 run(`c:\\tools\\git\\bin\\git.exe commit -m $commit_message`)
-run(`c:\\tools\\git\\bin\\git.exe push --all`)
+run(`c:\\tools\\git\\bin\\git.exe push --all`) #this is my fix today
 #new comment againa
